f8fc4f618da1755f257b7ec0a2ed4c238ee30d76
cadereynoldson <cader444@gmail.com>
2020-12-16 00:14:40 -0800

    test
    test

--- src/main/java/gui_v2/fadingComponents/FadeCapable.java
+++
@@ -1,7 +0,0 @@
-package gui_v2.fadingComponents;
-
-import java.util.ArrayList;
-
-public interface FadeCapable {
-    public ArrayList<FadingComponent> getFadingComponents();
-}
--- src/main/java/gui_v2/logic/FileOptions.java
+++
@@ -1,5 +0,0 @@
-package gui_v2.logic;
-
-public enum FileOptions {
-    ENTRIES, ITEMS, OUTPUT;
-}
